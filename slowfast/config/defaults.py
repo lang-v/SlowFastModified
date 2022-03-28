8e4790d426ee7f5464e9cece0c127f439408d0da
sanshuishi <sanshuishi@tencent.com>
2022-03-28 14:15:32 +0800

    更改标签显示，top-k

--- slowfast/config/defaults.py
+++ slowfast/config/defaults.py
@@ -912,7 +912,7 @@ _C.DEMO.STARTING_SECOND = 900
 _C.DEMO.FPS = 30
 # Visualize with top-k predictions or predictions above certain threshold(s).
 # Option: {"thres", "top-k"}
-_C.DEMO.VIS_MODE = "thres"
+_C.DEMO.VIS_MODE = "top-k"
 # Threshold for common class names.
 _C.DEMO.COMMON_CLASS_THRES = 0.7
 # Theshold for uncommon class names. This will not be
